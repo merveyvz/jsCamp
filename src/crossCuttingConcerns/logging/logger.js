27c83776daf45e239cc1a609b31c0f58e99a43c4
merveyvz <70059315+merveyvz@users.noreply.github.com>
2021-11-09 11:10:22 +0300

    9 Kasım

---
+++ src/crossCuttingConcerns/logging/logger.js
@@ -0,0 +1,21 @@
+export class BaseLogger{
+
+    log(data){
+        console.log("default logger: " + data)
+    }
+    
+}
+
+export class ElasticLogger extends BaseLogger{
+
+    log(data){
+        console.log("logged to elastic " + data)
+    }
+}
+
+export class MongoLogger extends BaseLogger{
+
+    log(data){
+        console.log("logged to mongo " + data)
+    }
+}
---
+++ src/models/user.js
@@ -0,0 +1,9 @@
+export default class User {
+    constructor(id, firstName, lastName, city) {
+        this.firstName = firstName
+        this.id = id
+        this.lastName = lastName
+        this.city = city
+    }
+
+}
